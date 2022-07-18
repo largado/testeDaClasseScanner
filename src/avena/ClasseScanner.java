d09bb0d280e03f5b766c7ed4a1f42283ef860740
largado <alexandrohipolito@gmail.com>
2022-07-18 17:35:42 -0300

    Initial commit

---
+++ src/avena/ClasseScanner.java
@@ -0,0 +1,15 @@
+package avena;
+
+import java.util.Scanner;
+
+public class ClasseScanner {
+
+    public static void main(String[] args){
+
+        Scanner escaner = new Scanner(System.in);
+        String variavel = escaner.next();
+        System.out.println(" o que foi digitado é: ");
+        System.out.println(variavel);
+
+    }
+}
